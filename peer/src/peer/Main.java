acc04036419a63ed60f62f93391a0cfd4414d636
B2 <alijahanshahi@Ali-Jahanshahis-MacBook-Pro.local>
2012-04-09 00:38:23 +0430

    peer@peer completed without peer2server

--- peer/src/peer/Main.java
+++ peer/src/peer/Main.java
@@ -8,6 +8,7 @@ package peer;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 /**
  *
  * @author alijahanshahi
@@ -20,12 +21,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Integer serverPort = new Integer(args[0]);
+                        Scanner n = new Scanner(System.in);
+                    int serverPort =n.nextInt();;
+//                    sy
         String ip="localhost";//args[0];
         ServerSocket ListenSocket = null;
         try {
             ListenSocket = new ServerSocket(serverPort);
-            System.out.println("Server is listening at port: "+ serverPort );
+            System.out.println("Server is listening on port: "+ serverPort );
         } catch (IOException ex) {
             System.out.println("ServerSocket problem");
         }
@@ -34,7 +37,7 @@ public class Main {
             Socket s=null;
             try{
                  s=ListenSocket.accept();
-                 PeerHandler h=new PeerHandler(s,ip);
+                 PeerHandler h=new PeerHandler(s,ip,serverPort);
             }
             catch(Exception e){
                 System.out.println("Socket problem");
@@ -49,14 +52,16 @@ public class Main {
 class PeerHandler extends Thread{
     Socket skt;
     String ip;
+    Integer mainPort;
     DataInputStream input;
     DataOutputStream output;
     FileInputStream reader;
     FileOutputStream writer;
-    public PeerHandler(Socket s,String ip) {
+    public PeerHandler(Socket s,String ip,int port) {
         try {
             skt = s;
             this.ip=ip;
+            mainPort = new Integer(port+1);
             input = new DataInputStream(skt.getInputStream());
             output = new DataOutputStream(skt.getOutputStream());
         } catch (IOException ex) {
@@ -81,9 +86,7 @@ class PeerHandler extends Thread{
             }
             String massage = new String(request);
             String[] parameters= massage.split(";");
-            for (int i=0;i<parameters.length;i++)
-                System.out.println(parameters[i]);
-            System.out.println("massage from: " +skt.getInetAddress()+" massage: "+parameters[0]);
+            System.out.println("massage from: " +skt.getInetAddress()+" massage: "+massage);
             //response recieved from server
             // 0-> no file found
             // 1-> parameters are: ip-port-filename
@@ -93,16 +96,20 @@ class PeerHandler extends Thread{
                     //and downloading file from connection
                     int r=0;
                     Integer peerPort=new Integer(parameters[3]);
+                    System.out.println("port:" +peerPort);
                     byte[] send=new byte[1024];
                     String df=new String("give-me;");
+                    df = df.concat(ip+";"+mainPort.toString()+";");
                     Socket download=new Socket(parameters[2],peerPort );
                     DataOutputStream requestForDownload = new DataOutputStream(download.getOutputStream());
                     df=df.concat(parameters[4]);
+                    System.out.println("sent massage : "+df);
+                    System.out.println("ip & port: "+ip+" "+mainPort);
                     for(int i=0;i<df.length();i++)
                         send[i]=(byte)df.charAt(i);
                     requestForDownload.write(send);
                     System.out.println("Download request sent to : " + parameters[2] + " for downloading file: "+parameters[4]);
-                    Downloader downloader= new Downloader(skt, parameters[4]);
+                    Downloader downloader= new Downloader( parameters[4],mainPort);
                     return;
                 }
                 else{
@@ -111,7 +118,7 @@ class PeerHandler extends Thread{
             }
             //here peer must send requested file
             else if (parameters[0].startsWith("give-me")){
-                Sender sender = new Sender(skt, parameters[1]);
+                Sender sender = new Sender(parameters[1],parameters[2],parameters[3]);
             }
             else{
                 System.out.println("Wrong massage format!");
@@ -127,54 +134,74 @@ class PeerHandler extends Thread{
 
 
 class Downloader {
-    Socket skt;
+    int ListenPortForFile;
+    ServerSocket listenSocket;
+    Socket recieveSocket;
     FileOutputStream writer;
     DataInputStream input;
     String fileName;
-    public Downloader(Socket s,String fileName) {
-        skt = s;
+    public Downloader(String fileName, int port) {
         this.fileName = fileName;
+        ListenPortForFile = port;
         try {
-            input = new DataInputStream(skt.getInputStream());
-            writer = new FileOutputStream("2.txt");
+            listenSocket = new ServerSocket(ListenPortForFile);
+        }
+        catch (IOException ex) {
+            System.out.println("IO: listenSocket for reciving file in downloader class has problem");
+        }
+        try {
+            recieveSocket = listenSocket.accept();
+        }
+        catch (IOException ex) {
+            System.out.println("IO: recieveSocket for reciving file in downloader class has problem");
+        }
+        try {
+            input = new DataInputStream(recieveSocket.getInputStream());
+            writer = new FileOutputStream("downloaded "+fileName);
         }
         catch (IOException ex) {
             System.out.println("IO: downloader has problem");
         }
-        run();
+        Download();
+        try {
+            listenSocket.close();
+        } catch (IOException ex) {
+            System.out.println("Closing listenSocket on reciever peer has a problem!");
+        }
     }
-    private void run(){
+    private void Download(){
 
             int t = 0,c = 0;
             byte[] data= new byte[1024];
             try {
-                while ((t = input.read(data)) >= 0) {
-                    writer.write(data);
-                    System.out.println(c);
-                    c++;
+                while ((t = input.read(data)) >=0 ){
+                    writer.write(data, 0 , t);
                 }
-                System.out.println("Downloading "+fileName+" from "+skt.getInetAddress()+" finishish successfully!");
+                System.out.println("Downloading "+fileName+" finished successfully!");
             }
-            catch (IOException ex) {
+            catch (Exception ex) {
                 System.out.println("IO: downloader in while a problem accured");
             }     
     }
 }
 
 
-class Sender extends Thread{
-    Socket sendSocket;
+class Sender {
+    private Socket sendSocket;
     private String filename;
+    private  Integer port;
+    private String ip;
     private FileInputStream reader;
     private DataOutputStream sender;
-    Socket s;
-    public Sender(Socket s, String fileName) {
-        sendSocket=s;
+    public Sender(String ip, String port, String fileName) {
         this.filename = fileName;
+        this.port = new Integer(port);
+        this.ip = ip;
         try {
+            sendSocket = new Socket(ip, this.port);
             sender = new DataOutputStream(sendSocket.getOutputStream());
         } catch (IOException ex) {
-            System.out.println("IO: Getting socketOututStream has a problem in Sender class!");
+            System.out.println("IO: newing socketOututStream has a problem in Sender class!");
         }
 
         try {
@@ -183,18 +210,16 @@ class Sender extends Thread{
         } catch (FileNotFoundException ex) {
            System.out.println("FileNotFound: Newing fileInputStream has a problem in Sender class!");
         }
-        this.start();
+        Send();
     }
-    @Override
-    public void run(){
+    public void Send(){
         try {
             int cnt = 0,c=0;
             byte[] data = new byte[1024];
             while ((cnt = reader.read(data)) >= 0) {
-                sender.write(data);
-                System.out.println(c);
-                c++;
+                sender.write(data, 0, cnt);
             }
+            sender.close();
             System.out.println("Sending File: "+filename+" To "+sendSocket.getInetAddress() + " finished successfully!");
         }
         catch (IOException ex) {
